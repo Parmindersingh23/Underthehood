b867904b726a68d9eeb9649669aee4f16e87b11d
Alicia Quezada & Parminder Singh <pair+aqueza2.parmindersingh23@devbootcamp.com>
2016-01-06 16:55:28 -0600

    all routes working almost

---
+++ app/controllers/posts.rb
@@ -0,0 +1,35 @@
+get '/posts' do
+  @posts = Post.all
+  erb :index
+end
+
+get '/posts/new' do
+  erb :'/posts/new'
+end
+
+get '/posts/:id' do
+  @post = Post.find(params[:id])
+  erb :post
+end
+
+#parminder/alicia created this
+post '/new' do
+  @post = Post.create(title: params[:title], content: params[:content])
+  redirect '/posts'
+end
+
+delete '/posts/:id' do
+  # write logic for deleting posts here.
+end
+
+get '/posts/:id/vote' do
+  post = Post.find(params[:id])
+  post.votes.create(value: 1)
+  redirect "/posts"
+end
+
+get '/posts/:id/downvote' do
+  post = Post.find(params[:id])
+  post.votes.create(value: -1)
+  redirect "/posts"
+end
