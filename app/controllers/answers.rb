9471cb118cff24a167a10ec1bb22c057d37dca68
Alicia Quezada & Parminder Singh <pair+aqueza2.parmindersingh23@devbootcamp.com>
2016-01-07 16:03:04 -0600

    append comments

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,1 +1,14 @@
-# get 'posts/:id/answers
+# get 'posts/:id/answers' do
+
+# end
+
+# get 'posts/:id/answers/new' do
+
+# end
+
+post '/posts/:id/answers' do
+  if request.xhr?
+    @answer = Answer.create(content: params[:description], post_id: params[:id])
+  end
+   erb :"answers/_show", layout: false
+end
